4d84131c9aae8ac8d369984f6a7c57a0db2a0dd5
feeshmaster <125420779+feeshmaster@users.noreply.github.com>
2026-01-14 14:44:37 -0600

    first commit

---
+++ mode_selector.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Minesweeper gamemode selection")
+root.geometry("800x600")
+
+canvas_width = 800
+canvas_height = 600
+canvas = tk.Canvas(root, 
+                   width=canvas_width, 
+                   height=canvas_height, 
+                   bg="black")
+
+canvas.pack(pady=20, padx=20)
+
+
+
+
+root.mainloop()
